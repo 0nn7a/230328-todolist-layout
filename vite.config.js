ff98ca5efafb532027f6e457c1c975e545f64664
0nn7a <mohonaney@gmail.com>
2023-03-29 17:09:03 +0800

    add base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/230328-todolist-layout/',
   plugins: [
     vue(),
     AutoImport({
